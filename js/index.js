a96c194f8e8676e5968b8b7cc52958a4f61c62df
Abril Da Pra <abydapra@gmail.com>
2023-11-24 11:09:04 -0300

    Actualizo proyectos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let check = document.querySelector(".check");
 
 let countElement = document.getElementById("count");
-let projectsCompleted = 7;
+let projectsCompleted = 8;
 let isVisible = false;
 let counterStarted = false;
 
