9543510499dc741498d6320fe2cb050ca34bf2b1
Abril <abydapra@gmail.com>
2024-02-29 22:20:46 -0300

    Arreglo proyecto portfolio

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let check = document.querySelector(".check");
 
 let countElement = document.getElementById("count");
-let projectsCompleted = 10;
+let projectsCompleted = 9;
 let isVisible = false;
 let counterStarted = false;
 
